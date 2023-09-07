30d1561d8b39a730611d4559d1e3e8a0aeaa6a76
TRStolsvik <trondstolsvik@gmail.com>
2023-09-07 13:28:10 +0200

    Script works
    Script written in three functions. Also added css to print even numbers in bold text.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,69 @@
-// Call Hello World!
-function HelloWorld() {
-  alert('Hello World!');
-}
+(function (app) {
+  'use strict';
+
+  app.benjamins = function () {
+    const submitButton = document.getElementById('btnSubmit');
+    submitButton.addEventListener('click', getValues);
+  };
+
+  function getValues() {
+    // Get startValue from the page.
+    let startValue = document.getElementById('startValue').value;
+    // Get endValue from the page.
+    let endValue = document.getElementById('endValue').value;
+
+    // Convert the strings that come back to integers.
+    startValue = Number.parseInt(startValue);
+    endValue = Number.parseInt(endValue);
+
+    // Check and verify that the strings are now in fact integers.
+    if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
+      // Call function generateNumbers with the returned values.
+      const numbers = generateNumbers(startValue, endValue);
+
+      // Call function displayNumbers to display the returned array of numbers on the page.
+      displayNumbers(numbers);
+    }
+  }
+
+  function generateNumbers(startValue, endValue) {
+    // Declare an empty array called numbers
+    let numbers = [];
+
+    // For-loop through the the range specified in the function getValues.
+    // We want to start the loop at startValue, and end at endValue, iterating over it one number at a time.
+    for (let i = startValue; i <= endValue; i++) {
+      // For each iteration, append the result to the array called numbers.
+      numbers.push(i);
+    }
+    // Finally return numbers.
+    return numbers;
+  }
+
+  function displayNumbers(numbers) {
+    // Declare a variable called templateRows and set it to an empty string ''.
+    // This variable is used to insert a new row for the table.
+    let templateRows = '';
+
+    // For-loop through numbers
+    for (let i = 0; i < numbers.length; i++) {
+      // Declare a variable called className and set it to even.
+      let className = 'even';
+
+      let number = numbers[i];
+
+      // For each even number, set className to even, else set className to odd.
+      if (number % 2 == 0) {
+        className = 'even';
+      } else {
+        className = 'odd';
+      }
+
+      // Concatenate (+=) each iteration to the variable called templateRows using string interpolation.
+      // Use the following syntax: templateRows += `<tr><td class="${className}">${number}</td></tr>`;
+      templateRows += `<tr><td class="${className}">${number}</td></tr>`;
+    }
+    // Finally, display the results in the html page using getElementById.
+    document.getElementById('results').innerHTML = templateRows;
+  }
+})((window.app = window.app || {}));
